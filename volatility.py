9ef3611d60dc9edb2fb66d50f3ddab162a0c6d0b
Joseph Sasin <jsasin1990@gmail.com>
2025-12-23 21:41:23 -0800

    Wip, In the middle of fixing swing_days

--- volatility.py
+++ volatility.py
@@ -6,9 +6,15 @@ vol = 0.02
 num_simulations = 10000
 days = 30
 
-shocks = np.random.normal(0.1, vol, (num_simulations, days))
+shocks = np.random.normal(0, vol, (num_simulations, days))
 
-multiplier = 1 + shocks
+
+swing = np.random.normal(0.05, vol, (num_simulations, days))
+swing_day = np.random.randint(1, 8)
+if swing_day == 1:
+    multipiler = 1 + swing
+else:
+    multiplier = 1 + shocks
 
 end_price = price * np.cumprod(multiplier, axis=1)
 row = end_price[:, -1]
@@ -17,4 +23,4 @@ average = np.mean(row)
 low = np.min(row)
 high = np.max(row)
 
-print(average, low, high)
+print(f"The average is: {average},\nThe lowest price is: {low},\nThe highest price is: {high}")
