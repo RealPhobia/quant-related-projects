4b5a4ef547cd98531140ed0df5c3c3af4bcd1b51
Joseph Sasin <jsasin1990@gmail.com>
2026-01-01 12:59:05 -0800

    Refactor: Finished Swing Days

--- volatility.py
+++ volatility.py
@@ -4,16 +4,25 @@ price = 100
 vol = 0.02
 num_simulations = 10000
 days = 30
+max_number_swing_days = int(days/4)
 
-shocks = np.random.normal(0, vol, (num_simulations, days))
+shocks = np.random.normal(0, vol, (num_simulations, days)) # actual matrix
 
-number_of_swing_days = np.random.randint(0, days)
-swing_day = np.random.randint(0, days)
-direction = np.random.choice([1, -1], num_simulations)
-shocks[:, swing_day] = np.random.normal(0.05 * direction, vol)
+num_of_swing_days = np.random.randint(0, max_number_swing_days) # determining number of swing days
+indices_of_swing_day = np.random.choice(days, num_of_swing_days, replace=False) #determining indices of swing_days
+
+direction = np.random.choice([1, -1], num_simulations) # swing day negetive or positive
+
+
+
+if num_of_swing_days > 0:
+    swing = 0.05 * direction[:, np.newaxis]
+
+    new_shocks = np.random.normal(swing, vol, (num_simulations, num_of_swing_days))
+
+    shocks[:, indices_of_swing_day] = new_shocks
 
 multiplier = 1 + shocks
-
 end_price = price * np.cumprod(multiplier, axis=1)
 row = end_price[:, -1]
 
